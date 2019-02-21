4030155f4624e7faeedec10a2e6d5d7bd8bf9225
Jeffrey <deckerjeffreyr@gmail.com>
2019-02-20 21:34:56 -0800

    Adding build for heroku deployment

---
+++ build/precache-manifest.859d6473dfbe398981daa77c9168d0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "26d9fa7725b2fbb9d7d2",
+    "url": "/static/js/main.26d9fa77.chunk.js"
+  },
+  {
+    "revision": "3b9c13a10a26f1fc1c70",
+    "url": "/static/js/2.3b9c13a1.chunk.js"
+  },
+  {
+    "revision": "26d9fa7725b2fbb9d7d2",
+    "url": "/static/css/main.160a29ad.chunk.css"
+  },
+  {
+    "revision": "b068c6702bc96b10d4350f147ba01a2e",
+    "url": "/index.html"
+  }
+];
