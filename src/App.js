de552dd9d350964e2aa7332c7e38958c58a57a52
Jeffrey <deckerjeffreyr@gmail.com>
2019-02-27 00:29:16 -0800

    Adding react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AppBar from './AppBar';
 import DraftRoom from './DraftRoom';
+import { Switch, Route, IndexRoute, Link, hashHistory } from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome!
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppBar />
-        <DraftRoom />
+      <div>
+      <AppBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/draft" component={DraftRoom} />
+      </Switch>
       </div>
     );
   }
--- src/AppBar.js
+++ src/AppBar.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function AppBar(props) {
   const { classes } = props;
   return (
     <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Leagues</Nav.Link>
-      <Nav.Link href="#features">Players</Nav.Link>
-    </Nav>
-  </Navbar>
+      <LinkContainer to={'/'}>      
+        <Navbar.Brand href="/">Navbar</Navbar.Brand>
+      </LinkContainer>
+      <Nav className="mr-auto">
+        <LinkContainer to={'/'}>      
+          <Nav.Link>Leagues</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to={'/draft'}>      
+          <Nav.Link >Draft</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </Navbar>
   );
 }
 
